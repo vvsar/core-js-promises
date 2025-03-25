5ddc845c9982c0716f12a5e6ae49d6824f1c0c16
Victor Saraev <vvsar@mail.ru>
2025-03-25 18:34:28 +0300

    feat: add getPromiseResult solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -38,8 +38,19 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    resolve(
+      source.then(
+        () => {
+          return 'success';
+        },
+        () => {
+          return 'fail';
+        }
+      )
+    );
+  });
 }
 
 /**
