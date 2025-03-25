2420b07df05c7bfab62b54652f6a3582579c6a18
Victor Saraev <vvsar@mail.ru>
2025-03-25 19:02:32 +0300

    feat: add getAllResult solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -120,8 +120,15 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.all(
+    promises.map((p) =>
+      Promise.resolve(p).then(
+        (value) => value,
+        () => null
+      )
+    )
+  );
 }
 
 /**
